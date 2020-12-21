3b91f7e0207c98614deea947a0ea0345c2db76ec
M.m.shahabaz Baig <bmmshaha@amazon.com>
2020-12-21 22:58:16 +0530

    adding contact page styling

--- script/page.js
+++ script/page.js
@@ -1,12 +1,12 @@
 console.log("LOcked and loaded externally");
 
-let links = document.querySelectorAll(".nav-link");
+let links = document.querySelectorAll(".nav-link a");
 links.forEach((link) => {
 
     link.addEventListener('click', (e) => {
 
         links.forEach((link) =>{
-            link.querySelector('a').style.borderBottom = "none";
+            link.style.borderBottom = "none";
             console.log("removing the border");
         })
         e.target.style.borderBottom = "2px solid rgb(64, 191, 191)";
