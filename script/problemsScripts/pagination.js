17221d9fc5bcc3ea404f5b80dcaa3c25930fe536
M.m.shahabaz Baig <bmmshaha@amazon.com>
2021-01-25 20:23:13 +0530

    adding porblems

---
+++ script/problemsScripts/pagination.js
@@ -0,0 +1,324 @@
+let listOfAddresses = [
+        {
+            id: 1,
+            name: 'P. Sherman',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 1
+        },
+        {
+            id: 2,
+            name: 'Emily Sherman',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 1
+        }, {
+            id: 3,
+            name: 'Kyle',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 1
+        }, {
+            id: 4,
+            name: 'someone else',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 1
+        },{
+            id: 5,
+            name: 'Emily Sherman',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 1
+        }, {
+            id: 6,
+            name: 'Kyle',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 2
+        }, {
+            id: 7,
+            name: 'someone else',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 2
+        },{
+            id: 8,
+            name: 'Emily Sherman',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 2
+        }, {
+            id: 9,
+            name: 'Kyle',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 2
+        }, {
+            id: 10,
+            name: 'someone else',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 2
+        },
+        {
+            id: 11,
+            name: 'P. Sherman',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 3
+        },
+        {
+            id: 12,
+            name: 'Emily Sherman',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 3
+        }, {
+            id: 13,
+            name: 'Kyle',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 3
+        }, {
+            id: 14,
+            name: 'someone else',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 3
+        },{
+            id: 15,
+            name: 'Emily Sherman',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 3
+        }, {
+            id: 16,
+            name: 'Kyle',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 4
+        }, {
+            id: 17,
+            name: 'someone else',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 4
+        },{
+            id: 18,
+            name: 'Emily Sherman',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 4
+        }, {
+            id: 19,
+            name: 'Kyle',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 5
+        }, {
+            id: 20,
+            name: 'someone else',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 5
+        },{
+            id: 21,
+            name: 'P. Sherman',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 5
+        },
+        {
+            id: 22,
+            name: 'Emily Sherman',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 5
+        }, {
+            id: 23,
+            name: 'Kyle',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 5
+        }, {
+            id: 24,
+            name: 'someone else',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 5
+        },{
+            id: 25,
+            name: 'Emily Sherman',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 5
+        }, {
+            id: 26,
+            name: 'Kyle',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 6
+        }, {
+            id: 27,
+            name: 'someone else',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 6
+        },{
+            id: 28,
+            name: 'Emily Sherman',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 6
+        }, {
+            id: 29,
+            name: 'Kyle',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 6
+        }, {
+            id: 30,
+            name: 'someone else',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 6
+        },{
+            id: 31,
+            name: 'P. Sherman',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 7
+        },
+        {
+            id: 32,
+            name: 'Emily Sherman',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 7
+        }, {
+            id: 33,
+            name: 'Kyle',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 7
+        }, {
+            id: 34,
+            name: 'someone else',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 7
+        },{
+            id: 35,
+            name: 'Emily Sherman',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 7
+        }, {
+            id: 36,
+            name: 'Kyle',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 8
+        }, {
+            id: 37,
+            name: 'someone else',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 8
+        },{
+            id: 38,
+            name: 'Emily Sherman',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 8
+        }, {
+            id: 39,
+            name: 'Kyle',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 9
+        }, {
+            id: 40,
+            name: 'someone else',
+            addressLine1: '42 Wallaby Way',
+            addressLine2: 'Sydney, NSW',
+            page: 9
+        }
+    
+    // hasNextPage: true
+    ]
+
+let pageNumber = 1;
+
+window.onload = () => {
+    console.log("locked and loaded");
+    renderAddress(1);
+    // getAddresses(pageNumber);
+}
+
+let leftArrow = document.querySelector('#prevAddress');
+let rightArrow = document.querySelector('#nextAddress');
+
+leftArrow.addEventListener('click', (e) => {
+    renderAddress(--pageNumber);
+});
+
+rightArrow.addEventListener('click', (e) => {
+    renderAddress(++pageNumber);
+})
+
+// let getAddresses = (page) => {
+
+//     let min = pageNumber;
+//     let max = pageNumber+5;
+
+// }
+
+let renderAddress = (page) => {
+    console.log(page);
+    let addresses = getAddressesFor(page);
+    console.log(addresses);
+    
+    console.log(createAddressDiv(addresses));
+    document.querySelector('.addresses').innerHTML = "";
+    createAddressDiv(addresses).forEach((div) => {
+        document.querySelector('.addresses').appendChild(div);
+    });
+}
+
+let getAddressesFor = (page) => {
+
+    return listOfAddresses.filter((item) => {
+        return item.page === page;
+    });
+
+    // console.log(currentAddress);
+}
+
+
+let createAddressDiv = (addresses) => {
+    
+    let addressDivs = addresses.map((address) => {
+        return createDiv(address);
+        
+    });
+
+    return addressDivs;
+}
+
+let createDiv = (address) => {
+    let div = document.createElement('div');
+    let p = document.createElement('p');
+    let pText = document.createTextNode(address.id);
+
+    p.appendChild(pText);
+    div.appendChild(p);
+    console.log(div);
+    return div;
+}
+
+
+
+
+
