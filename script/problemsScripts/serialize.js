37c6b5518c076701080d7178ac1630f63f618fcb
Ethan Hunt <shabazbaig5@gmail.com>
2021-01-26 18:24:40 +0530

    adding new problems

---
+++ script/problemsScripts/serialize.js
@@ -0,0 +1,124 @@
+
+
+window.onload = () => {
+
+    window.extraData = {
+        payment : {
+          voucher : ''
+        }
+      }
+    window.shippingData = {
+        street: {
+          name: 'testName',
+          number: 'testStreetnum',
+        },
+        zipcode: 'testZip',
+        contact: {
+          phone: {
+            area: 'testPhone',
+            exchange: 'testExchange',
+            subscriber: 'testSubs'
+          }
+        }
+      }
+
+    // let serializeData = serialize(document.querySelector('#shipping'));
+    populateForm = populate(shippingData);
+}
+
+
+
+populate = (data) => {
+
+    console.log(data);
+
+    for(let i in data){
+        
+
+      let obj = differentiate(data[i],i);
+      console.log(obj);
+        
+    }
+
+}
+
+
+differentiate = (dataSet, i) => {
+
+  if(typeof dataSet !== 'object'){
+    console.log(dataSet);
+    console.log(i);
+    // document.querySelector('#shipping').querySelector('input[type="name"]').value = dataSet;
+    document.querySelector('#shipping').querySelectorAll('input[type="text"]').forEach((inputField) => {
+      if(inputField.getAttribute('name') == i){
+        inputField.value = dataSet;
+      }
+    })
+    
+    // return i.toString();
+  }else{
+
+    for(let prop in dataSet){
+
+      // if(typeof dataSet == 'object'){
+
+        differentiate(dataSet[prop],i+'.'+prop);
+
+      // }
+    }
+
+  }
+  
+}
+
+
+serialize = (form) => {
+
+    console.log(form);
+    let inputs = form.querySelectorAll('input[type="text"]');
+    // console.log(inputs);
+    console.log(shippingData);
+    let obj = {};
+    inputs.forEach((input) => {
+
+        console.log(input.name);
+        // shippingData[input.name] = input.value;
+        set(obj, input.name, input.value);
+
+    });
+
+    console.log("final object  : ")
+    console.log(obj);
+
+    console.log(JSON.stringify(obj));
+    // console.log(shippingData)
+}
+
+
+set = (obj, key,value) => {
+
+    // console.log(obj, key, value);
+    // console.log(key);
+    let splitKeys = key.split('.');
+
+
+    for(let i = 0; i<splitKeys.length; i++){
+
+    
+        if(!obj.hasOwnProperty(splitKeys[i])){
+            obj[splitKeys[i]] = {}
+        }
+        if(i+1 == splitKeys.length){
+            obj[splitKeys[i]] = value;
+        }else{
+            obj = obj[splitKeys[i]];
+        }
+
+
+    }
+
+    // console.log(splitKeys[splitKeys.length-1]);
+    obj[splitKeys[splitKeys.length-1]] = value;
+
+    // console.log(obj);
+}
