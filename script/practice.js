9c166554385e67fe9b3fb4c5fadcf87d8e265685
M.m.shahabaz Baig <bmmshaha@amazon.com>
2021-02-23 23:31:34 +0530

    adding new practice code

---
+++ script/practice.js
@@ -0,0 +1,17 @@
+function Person(name,id){
+    // let name = "shahabaz";
+    // let id = "baig";
+    this.name = name;
+    this.id = id;
+    
+
+}
+
+
+
+let person1 = new Person("shahabaz", "baig");
+
+console.log(person1.proto);
+
+
+//prototype practice
